5ec2fc2bd5f3a8224b92698a8db05407a153d35f
myssynglynx <jaiq@MacBook-Pro.local>
2021-12-05 17:32:39 -0800

    encoded babel, deployed amplify, updated README

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,13 @@
-import React from 'react'
 import Main from '../main/main'
 
 import './app.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Main />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
 }
 
 export default App;
